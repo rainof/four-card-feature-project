0acbae2675ad60971e7d2bacbf55892e831b0fba
rainof <13416303+rainof@users.noreply.github.com>
2024-07-31 17:04:01 +0700

    update: modify the styling of boxes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import React from 'react';
 import './App.scss';
+
+import supervisorIcon from './assets/images/icon-supervisor.svg';
+import teamBuilderIcon from './assets/images/icon-team-builder.svg';
+import karmaIcon from './assets/images/icon-karma.svg';
+import calculatorIcon from './assets/images/icon-calculator.svg';
 
 function App({data}) {
   const {title, support, description, topics} = data;
+
+  const image_files = [
+    supervisorIcon,
+    teamBuilderIcon,
+    karmaIcon,
+    calculatorIcon
+  ];
 
   return (
     <div className="app">
@@ -11,10 +24,14 @@ function App({data}) {
         <p>{description}</p>
       </header>
       <section className="topic_box">
-        {topics.map((topic) => (
-          <div>
-            <h3>{topic.name}</h3>
+        {topics.map((topic, index) => (
+          <div key={index} className={`sub_topic sub_topic-${index % image_files.length}`}>
+            <div className="topic_name">{topic.name}</div>
             <p>{topic.description}</p>
+            <img
+              src={image_files[index % image_files.length]}
+              alt={`icon-${index + 1}`}
+            />
           </div>
         ))}
       </section>
