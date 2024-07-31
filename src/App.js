3497a0cfacbdd360b1a77153ac4f06ecf62d2854
rainof <13416303+rainof@users.noreply.github.com>
2024-08-01 00:15:49 +0700

    update: add fonts and finish implementing mobile design

--- src/App.js
+++ src/App.js
@@ -20,15 +20,14 @@ function App({data}) {
   return (
     <div className="app">
       <header className="app_header">
-        <h1>{title}</h1>
-        <h1>{support}</h1>
-        <p>{description}</p>
+        <h1 className="header_title">{title}</h1>
+        <h1 className="header_support">{support}</h1>
+        <p className="header_description">{description}</p>
       </header>
       <section className="topic_box">
         {topics.map((topic, index) => (
           <div key={index} className={className(`sub_topic sub_topic-${index % image_files.length}`)}>
             <div className="topic_name merge_1_2">{topic.name}</div>
-            {/* <div></div> */}
             <p className="topic_description merge_3_4">{topic.description}</p>
             <div></div>
             <img
