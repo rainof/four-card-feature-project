150abf5b12a46a546538e5ca904f69d34f19ac32
rainof <13416303+rainof@users.noreply.github.com>
2024-07-31 15:25:26 +0700

    feat: add data passing functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-function App() {
+function App({data}) {
+  const {title, support, description, topics} = data;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="app_header">
+        <h1>{title}</h1>
+        <h1>{support}</h1>
+        <p>{description}</p>
       </header>
+      <section className="topic_box">
+        {topics.map((topic) => (
+          <div>
+            <h3>{topic.name}</h3>
+            <p>{topic.description}</p>
+          </div>
+        ))}
+      </section>
     </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const appData = {
+  title: "Reliable, efficient delivery",
+  support: "Powered by Technology",
+  description: "Our Artificial Intelligence powered tools use millions of project data points to ensure that your project is successful",
+  topics: [
+    {
+      name: "Supervisor",
+      description: "Monitors activity to identify project roadblocks"
+    }, {
+      name: "Team Builder",
+      description: "Scans our talent network to create the optimal team for your project"
+    }, {
+      name: "Karma",
+      description: "Regularly evaluates our talent to ensure quality"
+    }, {
+      name: "Calculator",
+      description: "Uses data from past projects to provide better delivery estimates"
+    }
+  ]
+};
+
 root.render(
   <React.StrictMode>
-    <App />
+    <App data={appData} />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
